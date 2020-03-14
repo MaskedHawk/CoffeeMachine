dbccf4144dfb1d419be3b6759b16dc2a99ca7bcd
Alexis Bollia <alexis.bollia@spallian.com>
2020-03-14 11:16:12 +0100

    Adding a pricing system to the drink's generator

--- src/Order.js
+++ src/Order.js
@@ -1,26 +1,55 @@
 const drinks = {
-    TEA : "T",
-    COFFEE : "C",
-    CHOCOLATE : "H"
+    TEA : {
+        code: "T",
+        price: 0.4
+    },
+    COFFEE : {
+        code: "C",
+        price: 0.6
+    },
+    CHOCOLATE : {
+        code :"H",
+        price: 0.5
+    }
 };
 
 class Order {
 
-    constructor(
+    /**
+     * Generate a new order from client selection
+     *
+     * @param drinks - desired client drinks
+     * @param sugar - desired amoount of sugar
+     * @param money - provided money
+     */
+    constructor({
         drinks = "",
-        sugar = 0
-    ){
+        sugar = 0,
+        money = 0
+    }) {
         this.drinks = drinks;
         this.sugar = sugar;
-    };
+        this.money = money;
+    }
 
-    toInstruction(){
-        if(this.sugar == 0) {
-            return `${this.drinks}::`;
+    _isEnoughMoney() {
+        return (this.money >= this.drinks.price);
+    }
+
+    toInstruction() {
+
+        if (!this._isEnoughMoney()) {
+            let missingMoney = this.drinks.price - this.money;
+
+            return `M:${missingMoney} missing`;
         }
 
-        return `${this.drinks}:${this.sugar}:0`;
-    };
+        if (this.sugar == 0) {
+            return `${this.drinks.code}::`;
+        }
+
+        return `${this.drinks.code}:${this.sugar}:0`;
+    }
 }
 
 module.exports = {drinks, Order};
