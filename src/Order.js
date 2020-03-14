ea54e1e8dbf38e44b7d2a067afde87aa8d11479a
Alexis Bollia <alexis.bollia@spallian.com>
2020-03-14 11:16:12 +0100

    Adding a pricing system to the drink's generator

--- src/Order.js
+++ src/Order.js
@@ -1,25 +1,54 @@
 const drinks = {
-    TEA : "T",
-    COFFEE : "C",
-    CHOCOLATE : "H"
+    TEA : {
+        code: "T",
+        price: 0.4
+    },
+    COFFEE : {
+        code: "C",
+        price: 0.6
+    },
+    CHOCOLATE : {
+        code :"H",
+        price: 0.5
+    }
 };
 
 class Order {
 
-    constructor(
+    /**
+     * Generate a new order from client selection
+     *
+     * @param drinks - desired client drinks
+     * @param sugar - desired amoount of sugar
+     * @param money - provided money
+     */
+    constructor({
         drinks = "",
-        sugar = 0
-    ){
+        sugar = 0,
+        money = 0
+    }){
         this.drinks = drinks;
         this.sugar = sugar;
+        this.money = money;
     };
 
+    _isEnoughMoney(){
+        return (this.money >= this.drinks.price);
+    }
+
     toInstruction(){
-        if(this.sugar == 0) {
-            return `${this.drinks}::`;
+
+        if (!this._isEnoughMoney()) {
+            let missingMoney = this.drinks.price - this.money;
+
+            return `M:${missingMoney} missing`
         }
 
-        return `${this.drinks}:${this.sugar}:0`;
+        if (this.sugar == 0) {
+            return `${this.drinks.code}::`;
+        }
+
+        return `${this.drinks.code}:${this.sugar}:0`;
     };
 }
 
--- tests/coffeemachine.spec.js
+++ tests/coffeemachine.spec.js
@@ -1,30 +1,53 @@
 const {drinks, Order} = require('../src/Order.js');
 
 describe('Turns customers orders into drink maker instructions', () => {
-    it('Should return T:1:0 when order is 1 tea with 1 sugar', () => {
-        const command = new Order(
-            drinks.TEA,
-            1
-        ).toInstruction();
+    it('Should return T:1:0 when order is 1 tea with 1 sugar and enough money', () => {
+        const command = new Order({
+                drinks: drinks.TEA,
+                sugar: 1,
+                money: 0.4
+        }).toInstruction();
 
       expect(command).toEqual('T:1:0');
     });
 
-    it('Should return H:: when order is 1 chocolate without sugar', () => {
-        const command = new Order(
-            drinks.CHOCOLATE
-        ).toInstruction();
+    it('Should return H:: when order is 1 chocolate without sugar and enough money', () => {
+        const command = new Order({
+            drinks: drinks.CHOCOLATE,
+            money: 0.5
+        }).toInstruction();
 
         expect(command).toEqual('H::');
     });
 
     it('Should return C:2: when order is 1 coffee with two sugar', () => {
-        const command = new Order(
-            drinks.COFFEE,
-            2
-        ).toInstruction();
+        const command = new Order({
+            drinks: drinks.COFFEE,
+            sugar: 2,
+            money: 0.6
+        }).toInstruction();
 
         expect(command).toEqual('C:2:0');
+    });
+
+    it('Should return money missing when order is 1 coffee and not enough money', () => {
+        const command = new Order({
+            drinks: drinks.COFFEE,
+            sugar: 2,
+            money: 0.1
+        }).toInstruction();
+
+        expect(command).toEqual('M:0.5 missing');
+    });
+
+    it('Should return drinks even if there is exceed money', () => {
+        const command = new Order({
+            drinks: drinks.COFFEE,
+            sugar: 2,
+            money: 0.1
+        }).toInstruction();
+
+        expect(command).toEqual('M:0.5 missing');
     });
 });
 
